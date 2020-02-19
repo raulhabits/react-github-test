473e268a033a55b51cb56ae4ce18affcdc056871
raul <raulhabits@gmail.com>
2020-02-18 22:17:31 -0500

    Updates

---
+++ precache-manifest.e2fe9396c9a1683d0092c76c115803e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63971c767d54e114b4ffc216a5b7107",
+    "url": "/react-github-test/index.html"
+  },
+  {
+    "revision": "7ce6a5ef602b0a481612",
+    "url": "/react-github-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "944b9cf7615e1069e314",
+    "url": "/react-github-test/static/js/2.53a8c91c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-github-test/static/js/2.53a8c91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ce6a5ef602b0a481612",
+    "url": "/react-github-test/static/js/main.67c92874.chunk.js"
+  },
+  {
+    "revision": "2926512b2371cee4fca6",
+    "url": "/react-github-test/static/js/runtime-main.a7671b84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
